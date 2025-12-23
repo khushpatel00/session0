7880ae93b0b4e25ec7a5fa4b53cc3f6a1907feeb
khushpatel00 <patelxkhush@gmail.com>
2025-12-23 23:42:39 +0530

    (ccm) implement start menu functionality with toggle on click and key events

---
+++ src/script.js
@@ -0,0 +1,9 @@
+start = document.querySelector('#start');
+startMenu = document.querySelector('#start-menu');
+
+start.addEventListener('click', () => {
+    startMenu.classList.toggle('hidden');
+});
+document.addEventListener('keyup', (e) => {
+   if (e.key == 'Meta' || e.key == 'Super' || e.key == 'Alt' ) startMenu.classList.toggle('hidden');
+});
