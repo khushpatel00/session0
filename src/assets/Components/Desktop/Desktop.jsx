f0aac13fea811c3244ef7b2b24e6e61e5b2388be
khushpatel00 <patelxkhush@gmail.com>
2026-02-17 22:51:50 +0530

    added: Wallpaper, ActivityBar, Managed Folder Structure

---
+++ src/assets/Components/Desktop/Desktop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ActivityBar from './Systems/ActivityBar'
+function Desktop() {
+  return (
+    <div id='desktop' className='w-screen h-screen overflow-hidden'>
+
+        <ActivityBar />
+    </div>
+  )
+}
+
+export default Desktop
---
+++ src/assets/Components/Desktop/Systems/ActivityBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ActivityBar() {
+	return (
+		<>
+			<div className='fixed bottom-0 w-screen h-14 flex justify-center flex-wrap bg-zinc-900/50 backdrop-blur-lg'>
+				<img src="/archicon.png" alt="" className='max-h-full p-2 ' />
+			</div>
+		</>
+	)
+}
+
+export default ActivityBar
