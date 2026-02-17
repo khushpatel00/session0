a08fd512fbb5a41ec41d7c5115519202048c047d
khushpatel00 <patelxkhush@gmail.com>
2026-02-17 23:50:58 +0530

    added: window for Program, with window-dragging

--- src/assets/Components/Desktop/Desktop.jsx
+++ src/assets/Components/Desktop/Desktop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ActivityBar from './Systems/ActivityBar'
+import Window from './Windows/Window'
 function Desktop() {
-  return (
-    <div id='desktop' className='w-screen h-screen overflow-hidden'>
+    return (
+        <div id='desktop' className='w-screen h-screen relative overflow-hidden'>
 
-        <ActivityBar />
-    </div>
-  )
+            <ActivityBar />
+            <div className='absolute'>
+                <Window className='rounded-sm overflow-hidden'></Window>
+            </div>
+        </div>
+    )
 }
 
 export default Desktop
